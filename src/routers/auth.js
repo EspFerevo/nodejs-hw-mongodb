4509cdfc0639821206d08bedc29eef5e73e56632
EspFerevo <olekchekrenov@gmail.com>
2024-08-09 19:59:57 +0300

    req

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -3,13 +3,20 @@ import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { registerUserSchema } from '../validation/auth.js';
 import { registerUserController } from '../controllers/auth.js';
 import { validateBody } from '../midlewares/validateBody.js';
+import { loginUserSchema } from '../validation/auth.js';
+import { loginUserController } from '../controllers/auth.js';
 
 const router = Router();
-
+///
 router.post(
-  './register',
+  '/register',
   validateBody(registerUserSchema),
   ctrlWrapper(registerUserController),
 );
-
+///
+router.post(
+  '/login',
+  validateBody(loginUserSchema),
+  ctrlWrapper(loginUserController),
+);
 export default router;
