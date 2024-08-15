a91b2b62737c5f6793d2d1256eb6e9c83f9522dd
EspFerevo <olekchekrenov@gmail.com>
2024-08-15 19:16:31 +0300

    UPD

--- src/midlewares/validateBody.js
+++ src/midlewares/validateBody.js
@@ -2,13 +2,12 @@ import createHttpError from 'http-errors';
 
 export const validateBody = (schema) => async (req, res, next) => {
   try {
-    await schema.validateAsync(req.body, {
-      abortEarly: false,
-    });
+    await schema.validateAsync(req.body, { abortEarly: false });
     next();
   } catch (err) {
+    console.error('Validation error:', err.details);
     const error = createHttpError(400, 'Bad Request', {
-      errors: err.details,
+      errors: err.details.map(detail => detail.message),
     });
     next(error);
   }
