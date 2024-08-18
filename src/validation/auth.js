0a66ab8a7c0579554a538d4907be7e4ffc7c0d9c
EspFerevo <olekchekrenov@gmail.com>
2024-08-18 21:29:58 +0300

    Update auth.js

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -1,28 +1,35 @@
 import Joi from 'joi';
 
+// export const registerUserSchema = Joi.object({
+//   name: Joi.string().min(3).max(30).required().messages({
+//     'string.min': 'Name should have a minimum length of 3 characters',
+//     'string.max': 'Name should have a maximum length of 30 characters',
+//     'any.required': 'Name is required',
+//   }),
+//   email: Joi.string().email().required().messages({
+//     'string.email': 'Email must be a valid email address',
+//     'any.required': 'Email is required',
+//   }),
+//   password: Joi.string()
+//     .min(8) // Минимальная длина пароля
+//     .pattern(
+//       /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+//     ) // Сложность пароля
+//     .required()
+//     .messages({
+//       'string.min': 'Password should have a minimum length of 8 characters',
+//       'string.pattern.base':
+//         'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
+//       'any.required': 'Password is required',
+//     }),
+// });
+
+
 /// Валидация регистарции пользователя
 export const registerUserSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required().messages({
-    'string.min': 'Name should have a minimum length of 3 characters',
-    'string.max': 'Name should have a maximum length of 30 characters',
-    'any.required': 'Name is required',
-  }),
-  email: Joi.string().email().required().messages({
-    'string.email': 'Email must be a valid email address',
-    'any.required': 'Email is required',
-  }),
-  password: Joi.string()
-    .min(8) // Минимальная длина пароля
-    .pattern(
-      /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-    ) // Сложность пароля
-    .required()
-    .messages({
-      'string.min': 'Password should have a minimum length of 8 characters',
-      'string.pattern.base':
-        'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
-      'any.required': 'Password is required',
-    }),
+  name: Joi.string().min(3).max(20).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(2).max(12).required(),
 });
 
 /// Валидация входа пользователя
